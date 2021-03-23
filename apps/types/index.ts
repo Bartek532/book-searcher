99f5962f9a7cf0db8940f0b7945d6a4e0ab706e7
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-03-23 17:18:01 +0100

    add configuration and pick techs again

---
+++ apps/types/index.ts
@@ -0,0 +1,43 @@
+export type User = {
+  readonly id: number;
+  readonly name: string;
+  readonly email: string;
+  readonly password: string;
+};
+
+export type Filters = {
+  readonly id: number;
+  readonly name: string;
+  readonly author: string;
+  readonly slug: string;
+  readonly room: string;
+  readonly place: string;
+  readonly series: string;
+};
+
+export type Book = {
+  readonly id: number;
+  readonly name: string;
+  readonly author: string;
+  readonly img: string;
+  readonly slug: string;
+  readonly room: string;
+  readonly place: string;
+  readonly description: string;
+  readonly series: string;
+  readonly createdAt: string;
+  readonly updatedAt: string;
+  readonly UserBookRate: BookRate[];
+  readonly BookTag: BookTag[];
+};
+
+export type BookRate = {
+  readonly bookId: number;
+  readonly userId: number;
+  readonly rate: number;
+};
+
+export type BookTag = {
+  readonly bookId: number;
+  readonly tagName: string;
+};
