972eb51ebd6ef5b328d611e14341cd43d781095e
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-04-03 15:08:50 +0200

    remove password input component and add validation schema for register

--- apps/client/src/utils/validationSchemas.ts
+++ apps/client/src/utils/validationSchemas.ts
@@ -15,3 +15,22 @@ export const loginSchema = yup.object({
     )
     .required("Hasło jest wymagane."),
 });
+
+export const registerSchema = yup.object({
+  name: yup.string().required("Nazwa użytkownika jest wymagana."),
+  email: yup
+    .string()
+    .email("Wprowadź poprawny email.")
+    .required("Email jest wymagany."),
+  password: yup
+    .string()
+    .matches(
+      passwordRegex,
+      "Hasło musi zawierać dużą i małą literę, cyfrę, znak specjalny oraz mieć conajmniej 8 znaków.",
+    )
+    .required("Hasło jest wymagane."),
+  confirmPassword: yup
+    .string()
+    .required("Powtórz swoje hasło.")
+    .oneOf([yup.ref("password"), null], "Hasła się nie zgadzają."),
+});
